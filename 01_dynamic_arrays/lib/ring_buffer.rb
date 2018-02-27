d3d42730e8730f7ac3dfbd0c025e8f5331efbbae
Akram <azaiezakrem@gmail.com>
2018-02-27 10:29:09 -0800

    added ring buffer

--- 01_dynamic_arrays/lib/ring_buffer.rb
+++ 01_dynamic_arrays/lib/ring_buffer.rb
@@ -5,27 +5,28 @@ class RingBuffer
 
   def initialize
     @capacity = 8
-    @length = 0
     @store = StaticArray.new(@capacity)
-    @start_idx = 0
+    @length = 0
+    @start_index = 0
   end
 
   # O(1)
+  #Finds the index starting to count from the logical_index
   def [](index)
     check_index(index)
-    @store[index]
+    @store[(@start_index + index) % @capacity]
   end
 
   # O(1)
   def []=(index, val)
     check_index(index)
-    @store[index] = val
+    @store[(@start_index + index) % @capacity] = val
   end
 
   # O(1)
   def pop
-    raise "index out of bounds" unless @length > 0
-    popped = @store[@length-1]
+    raise "index out of bounds" if @length == 0
+    popped = self[@length - 1]
     @length -= 1
     popped
   end
@@ -33,37 +34,44 @@ class RingBuffer
   # O(1) ammortized
   def push(val)
     @length += 1
-    @store[@length-1] = val
+    resize! if @length > capacity
+    self[@length - 1] = val
   end
 
   # O(1)
   def shift
-    raise "index out of bounds" unless @length > 0
-    shifted = @store[@start_idx]
-    @start_idx += 1
+    raise "index out of bounds" if @length == 0
+    shifted = self[0]
+    self[0] = nil
     @length -= 1
+    @start_index = (@start_index + 1) % @capacity
     shifted
   end
 
   # O(1) ammortized
   def unshift(val)
-    resize! if @length == @capacity
-    (@length - 2).downto(0).each { |i| @store[i+1] = @store[i] }
-    @store[0] = val
+    @length += 1
+    resize! if @length > @capacity
+    @start_index -= 1
+    self[0] = val
   end
 
   protected
-  attr_accessor :capacity, :start_idx, :store
+  attr_accessor :capacity, :start_index, :store
   attr_writer :length
 
   def check_index(index)
-    raise "index out of bounds" unless index >= 0 && index < @length
+    if index < 0 || index >= @length
+      raise "index out of bounds"
+    end
   end
 
   def resize!
-    @capacity *= 2
-    new_buffer = StaticArray.new(@capacity)
-    @length.times { |i| new_buffer[i] = @store[i] }
-    @store = new_buffer
+    temp_capacity = @capacity * 2
+    store = StaticArray.new(temp_capacity)
+    @length.times{ |idx| store[idx] = self[idx] }
+    @store = store
+    @start_index = 0
+    @capacity = temp_capacity
   end
 end
